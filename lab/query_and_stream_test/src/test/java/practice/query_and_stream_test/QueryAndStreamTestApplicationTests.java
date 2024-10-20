f557cd3910defe642bcd558df6a29cda7cf37f5d
hongxeob <sjun2918@naver.com>
2024-10-20 16:50:54 +0900

    project init

---
+++ lab/query_and_stream_test/src/test/java/practice/query_and_stream_test/QueryAndStreamTestApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.query_and_stream_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryAndStreamTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
