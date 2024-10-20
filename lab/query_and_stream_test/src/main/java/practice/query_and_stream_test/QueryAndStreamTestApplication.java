e730c20318f32fef1c22c7d74ada16145fc446bf
hongxeob <sjun2918@naver.com>
2024-10-20 16:50:54 +0900

    project init

---
+++ lab/query_and_stream_test/src/main/java/practice/query_and_stream_test/QueryAndStreamTestApplication.java
@@ -0,0 +1,13 @@
+package practice.query_and_stream_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryAndStreamTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryAndStreamTestApplication.class, args);
+	}
+
+}
