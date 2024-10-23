9f42b21e144ab2448bf58ff7a826b6bf6b3fa434
hongxeob <sjun2918@naver.com>
2024-10-23 19:14:48 +0900

    add : 인프런 자바 고급1 -  문제(Test) - Thread 상속 - Runnable 구현 - Runnable 익명 클래스 구현 - 여러 스레드 사용

---
+++ Infrlean/java-adv1/src/trhead/start/test/StartTest1Main.java
@@ -0,0 +1,24 @@
+package trhead.start.test;
+
+import static util.MyLogger.log;
+
+public class StartTest1Main {
+	public static void main(String[] args) {
+		CounterThread counterThread = new CounterThread();
+		counterThread.start();
+	}
+
+	static class CounterThread extends Thread {
+		@Override
+		public void run() {
+			for (int i = 1; i <= 5; i++) {
+				log("value = " + i);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+	}
+}
