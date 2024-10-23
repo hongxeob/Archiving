de268916bdf5fdbe44046d3903e8bef4b1c491aa
hongxeob <sjun2918@naver.com>
2024-10-23 19:02:09 +0900

    add : 인프런 자바 고급1 -  Runnable을 만드는 다양한 방법 - 정적 중첩 클래스 사용 - 익명 클래스 사용 - 익명 클래스 변수 없이 직접 전달 - 람다

--- Infrlean/java-adv1/src/trhead/start/ManyThreadMainV1.java
+++ Infrlean/java-adv1/src/trhead/start/ManyThreadMainV1.java
@@ -5,15 +5,20 @@ import static util.MyLogger.log;
 public class ManyThreadMainV1 {
 	public static void main(String[] args) {
 		log("main() start");
-		HelloRunnable runnable = new HelloRunnable();
 
-		Thread thread1 = new Thread(runnable);
-		thread1.start();
-		Thread thread2 = new Thread(runnable);
-		thread2.start();
-		Thread thread3 = new Thread(runnable);
-		thread3.start();
+		MyRunnable runnable = new MyRunnable();
+		Thread thread = new Thread(runnable);
+		thread.start();
 
 		log("main() end");
+	}
+
+	// 정적 중첩 클래스 사용
+	static class MyRunnable implements Runnable {
+
+		@Override
+		public void run() {
+			log("run()");
+		}
 	}
 }
