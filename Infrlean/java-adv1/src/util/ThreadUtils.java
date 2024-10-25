3b3c8d6f8c2a1c8fb42f859e15a5ac61e7f031b9
hongxeob <sjun2918@naver.com>
2024-10-25 11:31:27 +0900

    add : 인프런 자바 고급1 -  예외 유틸 생성

---
+++ Infrlean/java-adv1/src/util/ThreadUtils.java
@@ -0,0 +1,14 @@
+package util;
+
+import static util.MyLogger.log;
+
+public abstract class ThreadUtils {
+	public static void sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			log("인터럽트 발생, " + e.getMessage());
+			throw new RuntimeException(e);
+		}
+	}
+}
