033f5d031a657cc6e7006342f54d17d23da2edc3
hongxeob <sjun2918@naver.com>
2024-10-26 17:28:51 +0900

    add : 인프런 자바 고급1 - join 문제

---
+++ Infrlean/java-adv1/src/trhead/control/test/JoinTest2Main.java
@@ -0,0 +1,32 @@
+package trhead.control.test;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class JoinTest2Main {
+	public static void main(String[] args) throws InterruptedException {
+		Thread t1 = new Thread(new MyTask(), "t1");
+		Thread t2 = new Thread(new MyTask(), "t2");
+		Thread t3 = new Thread(new MyTask(), "t3");
+
+		t1.start();
+		t2.start();
+		t3.start();
+
+		t1.join();
+		t2.join();
+		t3.join();
+
+		System.out.println("모든 스레드 실행 완료");
+	}
+
+	static class MyTask implements Runnable {
+		@Override
+		public void run() {
+			for (int i = 1; i <= 3; i++) {
+				log(i);
+				sleep(1000);
+			}
+		}
+	}
+}
