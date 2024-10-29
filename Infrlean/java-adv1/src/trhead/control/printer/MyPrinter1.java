d1faf1217b9169aa763d9cab0ac1890afc533bf4
hongxeob <sjun2918@naver.com>
2024-10-29 19:34:16 +0900

    add : 인프런 자바 고급1 -  프린터 만들기1

---
+++ Infrlean/java-adv1/src/trhead/control/printer/MyPrinter1.java
@@ -0,0 +1,50 @@
+package trhead.control.printer;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class MyPrinter1 {
+	public static void main(String[] args) {
+		Printer printer = new Printer();
+		Thread thread = new Thread(printer);
+		thread.start();
+
+		Scanner userInput = new Scanner(System.in);
+		while (true) {
+			log("프린터할 문서를 입력하세요. 종로 (q): ");
+			String input = userInput.nextLine();
+			if (input.equals("q")) {
+				printer.work = false;
+				break;
+			}
+			printer.addJob(input);
+		}
+	}
+
+	static class Printer implements Runnable {
+		volatile boolean work = true;
+		Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+		@Override
+		public void run() {
+			while (work) {
+				if (jobQueue.isEmpty()) {
+					continue;
+				}
+				String job = jobQueue.poll();
+				log("출력 시작: " + job + ", 대기 문서 : " + jobQueue);
+				sleep(3000);
+				log("출력 완료: " + job);
+			}
+			log("프린터 종료");
+		}
+
+		public void addJob(String input) {
+			jobQueue.offer(input);
+		}
+	}
+}
