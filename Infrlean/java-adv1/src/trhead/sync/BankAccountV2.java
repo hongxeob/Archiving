47fdf284a72145fa0ee3f77b84f65c955172a361
hongxeob <sjun2918@naver.com>
2024-11-06 14:40:24 +0900

    add : 인프런 자바 고급1 - 출금2 :  사용

---
+++ Infrlean/java-adv1/src/trhead/sync/BankAccountV2.java
@@ -0,0 +1,38 @@
+package trhead.sync;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV2 implements BankAccount {
+	private int balance;
+
+	public BankAccountV2(int initialBalance) {
+		this.balance = initialBalance;
+	}
+
+	@Override
+	public synchronized boolean withdraw(int amount) {
+		log("거래 시작 : " + getClass().getSimpleName());
+
+		// 잔고가 출금액보다 많으면 false
+		log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+		if (balance < amount) {
+			log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+			return false;
+		}
+
+		log("[검증 완료] 출금액:" + amount + ", 잔액: " + balance);
+		sleep(1000);
+
+		balance -= amount;
+		log("[출금 완료] 출금액:" + amount + ", 잔액: " + balance);
+
+		log("거래 종료");
+		return true;
+	}
+
+	@Override
+	public int getBalance() {
+		return balance;
+	}
+}
--- Infrlean/java-adv1/src/trhead/sync/BankMain.java
+++ Infrlean/java-adv1/src/trhead/sync/BankMain.java
@@ -5,7 +5,8 @@ import static util.ThreadUtils.sleep;
 
 public class BankMain {
 	public static void main(String[] args) throws InterruptedException {
-		BankAccountV1 account = new BankAccountV1(1000);
+//		BankAccountV1 account = new BankAccountV1(1000);
+		BankAccount account = new BankAccountV2(1000);
 
 		Thread t1 = new Thread(new WithDrawTask(account, 800), "t1");
 		Thread t2 = new Thread(new WithDrawTask(account, 800), "t2");
