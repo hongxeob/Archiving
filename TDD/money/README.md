958da2e9e1ad103c257aba53b3379dc80456bedc
hongxeob <sjun2918@naver.com>
2024-11-17 15:15:01 +0900

    add : MoneyTest make Green - Dollar 부작용 해결

--- TDD/money/README.md
+++ TDD/money/README.md
@@ -4,5 +4,5 @@
 - [ ] 5달러 + 10CHF = 10달러 (환율이 2:1 일 경우)
 - [x] 5달러 X 2 = 10달러
 - [ ] amount를 private으로 만들기
-- [ ] Dollar 부작용?
+- [x] Dollar 부작용?
 - [ ] Money 반올림?
--- TDD/money/src/test/kotlin/example/MoneyTest.kt
+++ TDD/money/src/test/kotlin/example/MoneyTest.kt
@@ -7,8 +7,10 @@ class MoneyTest {
     @Test
     fun testMultiplication() {
         val five = Dollar(5)
-        five.times(2)
-        assertEquals(10, five.amount)
+        var product = five.times(2)
+        assertEquals(10, product.amount)
+        product = five.times(3)
+        assertEquals(15, product.amount)
     }
 }
 
@@ -17,7 +19,5 @@ class Dollar(
 ) {
     var amount: Int = amount
 
-    fun times(multiplier: Int) {
-        this.amount *= multiplier
-    }
+    fun times(multiplier: Int): Dollar = Dollar(amount * multiplier)
 }
