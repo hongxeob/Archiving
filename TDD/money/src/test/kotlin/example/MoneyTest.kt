ee77b90bf1eb2f73de8197b1894c3e2e31503878
hongxeob <sjun2918@naver.com>
2024-11-17 14:47:29 +0900

    add : MoneyTest make Green - 돌아만 가게끔

--- TDD/money/src/test/kotlin/example/MoneyTest.kt
+++ TDD/money/src/test/kotlin/example/MoneyTest.kt
@@ -6,8 +6,15 @@ import kotlin.test.assertEquals
 class MoneyTest {
     @Test
     fun testMultiplication() {
-        val five = Dollar(5)
+        val five = Dollar(10)
         five.times(2)
         assertEquals(10, five.amount)
+    }
+}
+
+class Dollar(
+    val amount: Int,
+) {
+    fun times(multiplier: Int) {
     }
 }
