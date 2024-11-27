b36262eb3703e5f299a8c95869200ac3cff59be8
hongxeob <sjun2918@naver.com>
2024-11-27 16:45:00 +0900

    add : 인프런 자바 고급1 - Lock Condition

--- Infrlean/java-adv1/src/trhead/bounded/BoundedMain.java
+++ Infrlean/java-adv1/src/trhead/bounded/BoundedMain.java
@@ -9,11 +9,11 @@ public class BoundedMain {
 	public static void main(String[] args) {
 		// 1.BoundedQueue 선택
 //		BoundedQueue queue = new BoundedQueueV1(2);
-		BoundedQueue queue = new BoundedQueueV3(2);
+		BoundedQueue queue = new BoundedQueueV4(2);
 
 		// 2.생산자, 소비자 실행 순서 선택, 반드시 하나만 선택!
-		producerFirst(queue); // 생산자 먼저 실행
-//		consumerFirst(queue); // 소비자 먼저 실행
+//		producerFirst(queue); // 생산자 먼저 실행
+		consumerFirst(queue); // 소비자 먼저 실행
 	}
 
 	private static void producerFirst(BoundedQueue queue) {
