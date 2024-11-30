055cb7baa7c37537724478301f8d23d82dc8aa46
hongxeob <sjun2918@naver.com>
2024-11-30 13:34:11 +0900

    add: 리팩토링 1판 - 예제1 : 임시변수 제거

--- Book/refactoring/src/main/java/practice/ch1/Customer.java
+++ Book/refactoring/src/main/java/practice/ch1/Customer.java
@@ -20,27 +20,37 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
         String result = getName() + " 고객님의 대여기록 \n";
-        int frequentRenterPoints = 0;
         for (int i = 0; i < rentals.size(); i++) {
             Rental rental = rentals.get(i);
 
-            // 적립 포인트 1 증가
-            frequentRenterPoints += rental.getFrequentRenterPoints();
-
             // 대여하는 비디오 정보와 대여료 출력
             result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
-
             //현재까지 누적된 총 대여료
-            totalAmount += rental.getCharge();
 
         }
 
         // 푸터 행 추가
-        result += "누적 대여료: " + String.valueOf(totalAmount) + "\n";
-        result += "적립 포인트: " + String.valueOf(frequentRenterPoints);
+        result += "누적 대여료: " + String.valueOf(getTotalCharge()) + "\n";
+        result += "적립 포인트: " + String.valueOf(getTotalFrequentRenterPoints());
         return result;
     }
 
+    private double getTotalCharge() {
+        double totalAmount = 0;
+        for (int i = 0; i < rentals.size(); i++) {
+            Rental rental = rentals.get(i);
+            totalAmount += rental.getCharge();
+        }
+        return totalAmount;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int frequentRenterPoints = 0;
+        for (int i = 0; i < rentals.size(); i++) {
+            Rental rental = rentals.get(i);
+            frequentRenterPoints += rental.getFrequentRenterPoints();
+        }
+        return frequentRenterPoints;
+    }
 }
