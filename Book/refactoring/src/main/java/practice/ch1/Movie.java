b9d6bc69b298bf5d0f3f86777afe9752ad5db455
hongxeob <sjun2918@naver.com>
2024-12-03 21:56:31 +0900

    add: 리팩토링 1판 - 예제1 : switch 문 수정 - Customer -> Rental -> Movie

--- Book/refactoring/src/main/java/practice/ch1/Movie.java
+++ Book/refactoring/src/main/java/practice/ch1/Movie.java
@@ -28,4 +28,33 @@ public class Movie {
     public void setPriceCode(int priceCode) {
         this.priceCode = priceCode;
     }
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case REGULAR -> {
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+
+            }
+            case CHILDREN -> {
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+            }
+            case NEW_RELEASE -> result += daysRented * 3;
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        //최신물을 2일이상 대여하면 보너스 포인트 지급
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            return 2;
+        }
+        return 1;
+    }
 }
--- Book/refactoring/src/main/java/practice/ch1/Rental.java
+++ Book/refactoring/src/main/java/practice/ch1/Rental.java
@@ -1,7 +1,7 @@
 package practice.ch1;
 
 public class Rental {
-    private Movie movie;
+    Movie movie;
     private int daysRented;
 
     public Rental(Movie movie, int daysRented) {
@@ -18,31 +18,10 @@ public class Rental {
     }
 
     public double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR -> {
-                result += 2;
-                if (getDaysRented() > 2) {
-                    result += (getDaysRented() - 2) * 1.5;
-                }
-
-            }
-            case Movie.CHILDREN -> {
-                result += 1.5;
-                if (getDaysRented() > 3) {
-                    result += (getDaysRented() - 3) * 1.5;
-                }
-            }
-            case Movie.NEW_RELEASE -> result += getDaysRented() * 3;
-        }
-        return result;
+        return movie.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints() {
-        //최신물을 2일이상 대여하면 보너스 포인트 지급
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
-            return 2;
-        }
-        return 1;
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
