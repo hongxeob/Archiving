7a2c6750e2b3676e77611121fdef0ea3485a03a4
hongxeob <sjun2918@naver.com>
2024-12-03 22:30:25 +0900

    add: 리팩토링 1판 - 예제1 : 다형성 적용을 이용한 추상화 - Price (NEW,CHILDREN,REGULAR)

---
+++ Book/refactoring/src/main/java/practice/ch1/price/NewReleasePrice.java
@@ -0,0 +1,19 @@
+package practice.ch1.price;
+
+import practice.ch1.Movie;
+
+public class NewReleasePrice extends Price {
+    @Override
+    public int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+
+    @Override
+    public double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2 : 1;
+    }
+}
