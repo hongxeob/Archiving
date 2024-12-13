7aa7557e3457118bfcf681de2d917a78ff1c8181
hongxeob <sjun2918@naver.com>
2024-12-13 10:16:34 +0900

    chore: TDD로 구현하는 디자인 패턴 시작

---
+++ TDD/design-patterns/test/strategy/PayStrategyTest.java
@@ -0,0 +1,21 @@
+package strategy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PayStrategyTest {
+
+    @Test
+    @DisplayName("asdf")
+    void asdf() throws Exception {
+
+        //given
+
+
+        //when
+
+        //then
+
+    }
+
+}
