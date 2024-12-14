aef2ff57b9d27d8ef909df9714d16a7d730071b0
hongxeob <sjun2918@naver.com>
2024-12-14 14:21:33 +0900

    feat : 카드 결제 구현 및 테스트

--- TDD/design-pattern/src/test/java/practice/behavioral/strategy/PayByCreditCard.java
+++ TDD/design-pattern/src/test/java/practice/behavioral/strategy/PayByCreditCard.java
@@ -1,13 +1,37 @@
 package practice.behavioral.strategy;
 
+import practice.behavioral.strategy.details.CreditCardDetails;
+import practice.behavioral.strategy.details.PaymentDetails;
+
+import java.util.logging.Logger;
+
 public class PayByCreditCard implements PayStrategy {
+    private CreditCard card;
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
     @Override
     public boolean pay(int paymentAmount) {
-        return true;
+        if (cardIsPresent()) {
+            logger.info("결제액 => " + paymentAmount);
+            card.updateAmount(card.getAmount() - paymentAmount);
+            logger.info("잔여 금액 => " + card.getAmount());
+            return true;
+        }
+        return false;
     }
 
     @Override
-    public void collectPaymentDetails(String email, String password) {
+    public void collectPaymentDetails(PaymentDetails paymentDetails) {
+        if (!(paymentDetails instanceof CreditCardDetails)) {
+            throw new IllegalArgumentException("Payment details must be of type CreditCardDetails");
+        }
+        CreditCardDetails creditCardDetails = (CreditCardDetails) paymentDetails;
+        card = new CreditCard(creditCardDetails.getNumber(), creditCardDetails.getDate(), creditCardDetails.getCsv());
+        logger.info("카드 설정 성공");
+        // 카드 유효성 검증등....
+    }
 
+    private boolean cardIsPresent() {
+        return card != null;
     }
 }
