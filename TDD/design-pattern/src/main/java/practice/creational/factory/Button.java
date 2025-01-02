1aca5c14285d88ee6d5977503d5be7bf26258058
hongxeob <sjun2918@naver.com>
2025-01-02 10:17:21 +0900

    add : 팩토리 패턴 세팅2

--- TDD/design-pattern/src/main/java/practice/creational/factory/Button.java
+++
@@ -1,7 +0,0 @@
-package practice.creational.factory;
-
-public interface Button {
-    void render();
-
-    void onClick();
-}
---
+++ TDD/design-pattern/src/test/java/practice/creational/factory/PizzaFactoryTest.java
@@ -0,0 +1,73 @@
+package practice.creational.factory;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PizzaFactoryTest {
+
+    private PizzaFactory pizzaFactory = new PizzaFactory();
+
+    @Test
+    @DisplayName("치즈 피자를 만들 수 있다.")
+    void createCheesePizza() throws Exception {
+
+        //given
+        String type = "cheese";
+
+        //when
+        Pizza pizza = pizzaFactory.createPizza(type);
+
+        //then
+        assertThat(pizza instanceof CheesePizza).isEqualTo(true);
+        assertThat(pizza.getPrice()).isEqualTo(10000);
+        assertThat(pizza.getName()).isEqualTo("cheese");
+    }
+
+
+    private interface Pizza {
+
+        double getPrice();
+
+        String getName();
+    }
+
+    private class CheesePizza implements Pizza {
+        @Override
+        public double getPrice() {
+            return 10000;
+        }
+
+        @Override
+        public String getName() {
+            return "cheese";
+        }
+    }
+
+    private class PizzaFactory {
+        public Pizza createPizza(String type) {
+            switch (type) {
+                case "cheese":
+                    return new CheesePizza();
+                case "peperoni":
+                    return new PeperoniPizza();
+                default:
+                    throw new IllegalArgumentException("알 수 없는 피자 종류입니다.");
+            }
+
+        }
+    }
+
+    private class PeperoniPizza implements Pizza {
+        @Override
+        public double getPrice() {
+            return 20000;
+        }
+
+        @Override
+        public String getName() {
+            return "peperoni";
+        }
+    }
+}
