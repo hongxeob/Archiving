4dc4c1b4d626303d2ce688126ed34c0bf2249500
hongxeob <sjun2918@naver.com>
2025-01-06 12:55:03 +0900

    add: 디자인 패턴 - 복합체 패턴 2. Directory 클래스 TDD

---
+++ TDD/design-pattern/src/test/java/practice/structural/compostie/Directory.java
@@ -0,0 +1,46 @@
+package practice.structural.compostie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Directory implements FileSystemComponent {
+    private String name;
+    private List<FileSystemComponent> components = new ArrayList<>();
+
+    public Directory(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name must not be null or empty");
+        }
+        this.name = name;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Directory: " + name);
+        System.out.println("Contents:");
+        for (FileSystemComponent component : components) {
+            component.showDetails();
+        }
+    }
+
+    @Override
+    public long getSize() {
+        long totalSize = 0L;
+        for (FileSystemComponent component : components) {
+            totalSize += component.getSize();
+        }
+        return totalSize;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<FileSystemComponent> getComponents() {
+        return components;
+    }
+
+    public void addComponent(FileSystemComponent component) {
+        components.add(component);
+    }
+}
