00f3cdeb82157286250e048f3d3b06013d9a78f7
hongxeob <sjun2918@naver.com>
2025-02-19 13:09:41 +0900

    add: 아파치 카프카 - 실습 init

--- Book/아파치 카프카 애플리케이션/practice/src/main/java/practice/kafka/PracticeApplication.java
+++ Book/아파치 카프카 애플리케이션/practice/src/main/java/practice/kafka/PracticeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaApplication {
+public class PracticeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KafkaApplication.class, args);
+        SpringApplication.run(PracticeApplication.class, args);
     }
 
 }
