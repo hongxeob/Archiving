dac877121b66b19d9896a92e1ee3a08093e9e787
hongxeob <sjun2918@naver.com>
2025-02-25 12:56:43 +0900

    add: 아파치 카프카 - 3장 : 프로듀서 API

---
+++ Book/아파치 카프카 애플리케이션/practice/src/main/java/practice/kafka/producer/CustomPartitioner.java
@@ -0,0 +1,36 @@
+package practice.kafka.producer;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.errors.InvalidPartitionsException;
+import org.apache.kafka.common.utils.Utils;
+
+import java.util.List;
+import java.util.Map;
+
+public class CustomPartitioner implements Partitioner {
+    @Override
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
+        if (keyBytes == null) {
+            throw new InvalidPartitionsException("Need Message Key");
+        }
+        if (((String) key).equals("hong")) {
+            return 0;
+        }
+        List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
+        int numPartitions = partitions.size();
+
+        return Utils.toPositive(Utils.murmur2(keyBytes) % numPartitions);
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+
+    }
+}
