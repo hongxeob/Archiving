6436a1ce2e55a533c0d0ea338f30f1df94927c05
hongxeob <sjun2918@naver.com>
2025-03-05 13:09:27 +0900

    add: 아파치 카프카 - 3장 실습 : 컨슈머 API

---
+++ Book/아파치 카프카 애플리케이션/practice/src/main/java/practice/kafka/consumer/Consumer.java
@@ -0,0 +1,38 @@
+package practice.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class Consumer {
+    private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
+    private static final String TOPIC = "test";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
+    private static final String GROUP_ID = "test-group";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put("bootstrap.servers", BOOTSTRAP_SERVERS);
+        configs.put("group.id", GROUP_ID);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
+        consumer.subscribe(Arrays.asList(TOPIC));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("{}", record);
+            }
+        }
+    }
+}
