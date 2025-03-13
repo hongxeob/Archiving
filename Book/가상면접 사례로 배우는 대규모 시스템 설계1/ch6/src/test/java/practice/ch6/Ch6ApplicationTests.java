d827c24f8c75e15e423ef79c938991e9afb5f718
hongxeob <sjun2918@naver.com>
2025-03-13 11:30:40 +0900

    add: system-interview : 6장 세팅

---
+++ Book/가상면접 사례로 배우는 대규모 시스템 설계1/ch6/src/test/java/practice/ch6/Ch6ApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.ch6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
