f0e65daed2cf221710c1bfd23c223522278c615a
hongxeob <sjun2918@naver.com>
2025-03-15 10:48:44 +0900

    add: 디자인 패턴 - 템플릿 메서드 패턴 - Network 추상 클래스 - Twitter 구현 클래스 구현

--- TDD/design-pattern/src/main/java/practice/behavioral/templatemethod/network/Twitter.java
+++ TDD/design-pattern/src/main/java/practice/behavioral/templatemethod/network/Twitter.java
@@ -1,4 +1,47 @@
 package practice.behavioral.templatemethod.network;
 
-public class Twitter {
+public class Twitter extends Network {
+    private java.util.logging.Logger log = java.util.logging.Logger.getLogger(Twitter.class.getName());
+
+    public Twitter(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    @Override
+    boolean logIn(String userName, String password) {
+        log.info("=== 유저 로그인 정보 체크 ===");
+        log.info("=== 유저 이름 : " + this.userName);
+        System.out.print("Password: ");
+        for (int i = 0; i < this.password.length(); i++) {
+            System.out.print("*");
+        }
+        simulateNetworkLatency();
+        log.info("=== 트위터 로그인 성공 ===");
+        return true;
+    }
+
+    @Override
+    void logOut() {
+
+    }
+
+    @Override
+    boolean sendData(byte[] data) {
+        return false;
+    }
+
+    private void simulateNetworkLatency() {
+        try {
+            int i = 0;
+            System.out.println();
+            while (i < 10) {
+                System.out.print(".");
+                Thread.sleep(500);
+                i++;
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
--- TDD/design-pattern/src/test/java/practice/behavioral/templatemethod/network/TwitterTest.java
+++ TDD/design-pattern/src/test/java/practice/behavioral/templatemethod/network/TwitterTest.java
@@ -1,4 +1,44 @@
-import static org.junit.jupiter.api.Assertions.*;
-class NetworkTest {
-  
+package practice.behavioral.templatemethod.network;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TwitterTest {
+
+    private Twitter twitter;
+    private String id;
+    private String password;
+    private String message;
+
+    @BeforeEach
+    void setUp() {
+        id = "id";
+        password = "password";
+        message = "message";
+        twitter = new Twitter(id, password);
+    }
+
+    @Test
+    @DisplayName("포스트를 할 수 있다.")
+    void postSuccessTest() throws Exception {
+        //when
+        boolean result = twitter.post(message);
+
+        //then
+        assertThat(result).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("로그인을 할 수 있다.")
+    void logInSuccessTest() throws Exception {
+        //when
+        boolean result = twitter.logIn(id, password);
+
+        //then
+        assertThat(result).isEqualTo(true);
+    }
+
 }
