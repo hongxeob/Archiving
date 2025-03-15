6e0161798e2e2cf9d98613c60b0d1ec4683bf8c0
hongxeob <sjun2918@naver.com>
2025-03-15 10:48:44 +0900

    add: 디자인 패턴 - 템플릿 메서드 패턴 - Network 추상 클래스 - Twitter 구현 클래스 구현

--- TDD/design-pattern/src/main/java/practice/behavioral/templatemethod/network/Network.java
+++ TDD/design-pattern/src/main/java/practice/behavioral/templatemethod/network/Network.java
@@ -1,4 +1,25 @@
 package practice.behavioral.templatemethod.network;
 
-public class Network {
+public abstract class Network {
+    String userName;
+    String password;
+
+    public Network() {
+    }
+
+    public boolean post(String message) {
+        if (logIn(this.userName, this.password)) {
+            boolean result = sendData(message.getBytes());
+            logOut();
+            return result;
+        }
+        return false;
+    }
+
+    abstract boolean logIn(String userName, String password);
+
+    abstract void logOut();
+
+    abstract boolean sendData(byte[] data);
+
 }
