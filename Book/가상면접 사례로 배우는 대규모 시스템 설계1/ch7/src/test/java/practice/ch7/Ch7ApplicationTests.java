4b3ce4d0256dbd169203a115d295f2535f407488
hongxeob <sjun2918@naver.com>
2025-03-24 17:36:10 +0900

    add: 가상면접 사레로 배우는 대규모 시스템 설계 ch7 프로젝트

---
+++ Book/가상면접 사례로 배우는 대규모 시스템 설계1/ch7/src/test/java/practice/ch7/Ch7ApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.ch7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
