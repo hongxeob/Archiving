87b01986716f61346044e1f4a4c82e56ad73729f
hongxeob <sjun2918@naver.com>
2025-04-12 19:08:54 +0900

    add: 가상면접 8장

---
+++ Book/가상면접 사례로 배우는 대규모 시스템 설계1/ch8/src/main/java/com/ch8/Ch8Application.java
@@ -0,0 +1,13 @@
+package com.ch8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch8Application.class, args);
+    }
+
+}
---
+++ Book/가상면접 사례로 배우는 대규모 시스템 설계1/ch8/src/test/java/com/ch8/Ch8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
