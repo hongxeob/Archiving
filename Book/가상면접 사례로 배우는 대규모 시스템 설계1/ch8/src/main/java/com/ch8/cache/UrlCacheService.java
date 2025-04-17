3be31a03ef21a0921182ff725363df68e169cce7
hongxeob <sjun2918@naver.com>
2025-04-17 19:11:19 +0900

    add: 가상 면접 8장 - cache

--- Book/가상면접 사례로 배우는 대규모 시스템 설계1/ch8/src/main/java/com/ch8/cache/UrlCacheService.java
+++ Book/가상면접 사례로 배우는 대규모 시스템 설계1/ch8/src/main/java/com/ch8/cache/UrlCacheService.java
@@ -1,4 +1,23 @@
 package com.ch8.cache;
 
-public class urlCacheService {
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class UrlCacheService {
+    private final Map<String, String> cache = new ConcurrentHashMap<>();
+
+    public void put(String shortUrl, String originalUrl) {
+        cache.put(shortUrl, originalUrl);
+    }
+
+    public String get(String shortUrl) {
+        return cache.get(shortUrl);
+    }
+
+    public void clear() {
+        cache.clear();
+    }
 }
