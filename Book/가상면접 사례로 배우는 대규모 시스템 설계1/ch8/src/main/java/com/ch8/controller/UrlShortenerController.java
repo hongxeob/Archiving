706e454a0bd18e4626450ebd9c16b29c18a6a4d3
hongxeob <sjun2918@naver.com>
2025-04-20 19:19:16 +0900

    add: 가상 면접 8장 - controller

--- Book/가상면접 사례로 배우는 대규모 시스템 설계1/ch8/src/main/java/com/ch8/controller/UrlShortenerController.java
+++ Book/가상면접 사례로 배우는 대규모 시스템 설계1/ch8/src/main/java/com/ch8/controller/UrlShortenerController.java
@@ -1,9 +1,14 @@
 package com.ch8.controller;
 
+import com.ch8.controller.dto.ShortenUrlRequest;
+import com.ch8.controller.dto.ShortenUrlResponse;
 import com.ch8.service.UrlShortenerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -11,5 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class UrlShortenerController {
     private final UrlShortenerService urlShortenerService;
 
+    @PostMapping("/shorten")
+    public ResponseEntity<ShortenUrlResponse> shortenUrl(@RequestBody ShortenUrlRequest request) {
+        final String shortenUrl = urlShortenerService.shortenUrl(request.originUrl());
+        final ShortenUrlResponse response = new ShortenUrlResponse(shortenUrl, request.originUrl());
 
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("{shortUrl}")
+    public ResponseEntity<Void> redirectToOriginalUrl(@PathVariable String shortUrl) {
+        final String originalUrl = urlShortenerService.getOriginalUrl(shortUrl);
+
+        // 원본 URL이 없는 경우 404 Not Found 응답
+        if (originalUrl == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        // 302 Found 응답을 사용하여 리다이렉트
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create(originalUrl))
+                .build();
+    }
 }
