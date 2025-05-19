17064cdfd629bfeaa90d03645c253b38c8b3c039
hongxeob <sjun2918@naver.com>
2025-05-19 17:39:20 +0900

    start go

--- Go-Practice/practice/main.go
+++ Go-Practice/practice/main.go
@@ -1,25 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	n, err := fmt.Print("Hello, World!")
+	fmt.Println(n, err)
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
-	}
+	foo, _ := doFoo()
+	fmt.Println(foo)
 
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
+	for i := 0; i < 10; i++ {
+		fmt.Println("dooFoo", i+1, "times")
+		_, _ = doFoo()
 	}
+}
+
+func doFoo() (int, error) {
+	return fmt.Println("I'm in doFoo Function. Not in touFoo")
 }
