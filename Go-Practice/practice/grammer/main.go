2cdcb7375020750b2d430b3e9ab871e9ac30e0b1
hongxeob <sjun2918@naver.com>
2025-05-22 16:26:21 +0900

    go start

--- Go-Practice/practice/grammer/main.go
+++ Go-Practice/practice/grammer/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func lenAndUpper(name string) (length int, uppercase string) {
+	length = len(name)
+	upper := strings.ToUpper(name)
+
+	return length, upper
+}
+
+func main() {
+	length, uppercase := lenAndUpper("hello")
+	fmt.Println(length, uppercase)
 }
