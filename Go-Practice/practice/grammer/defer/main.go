ac4e66b028e3f37106e4fb2991d1676623c34e55
hongxeob <sjun2918@naver.com>
2025-05-23 11:33:22 +0900

    golang - defer

--- Go-Practice/practice/grammer/defer/main.go
+++ Go-Practice/practice/grammer/defer/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func lenAndUpper(name string) (length int, uppercase string) {
-	defer fmt.Println("I`m done") // function이 끝나고 이 코드를 실행시킨다.
+	defer fmt.Println("I`m done") // function이 끝나고 이 코드를 실행시킨다. (순서 바꿔도 될듯?)
 	length = len(name)
 	uppercase = strings.ToUpper(name)
 
