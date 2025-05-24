de4407a6619805622c24c5aecd959bf149ac4411
hongxeob <sjun2918@naver.com>
2025-05-24 16:37:53 +0900

    golang - defer

--- Go-Practice/practice/grammer/defer/main.go
+++ Go-Practice/practice/grammer/defer/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I`m done") // function이 끝나고 이 코드를 실행시킨다.
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+
+	return
+}
+func main() {
+	length, uppercase := lenAndUpper("hello")
+	fmt.Println(length, uppercase)
 }
