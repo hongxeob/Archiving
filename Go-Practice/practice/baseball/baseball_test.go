c0fbb6bc53e6bdaa26dc8f19eefd5cf0c73a675e
hongxeob <sjun2918@naver.com>
2025-05-29 10:31:24 +0900

    golang - baseball

--- Go-Practice/practice/baseball/baseball_test.go
+++ Go-Practice/practice/baseball/baseball_test.go
@@ -2,10 +2,17 @@ package baseball
 
 import "testing"
 
+type Game interface {
+}
+
 func TestCreateGame(t *testing.T) {
 	var game Game = CreateGame("123")
 
 	if game == nil {
 		t.Fatalf("게임 실행 불가")
 	}
+}
+
+func CreateGame(nums string) Game {
+	return nil
 }
