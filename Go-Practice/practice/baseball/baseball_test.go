dbff46b3688497f8cfead402b86eeb196c60342f
hongxeob <sjun2918@naver.com>
2025-05-30 10:40:09 +0900

    add : nested if

--- Go-Practice/practice/baseball/baseball_test.go
+++ Go-Practice/practice/baseball/baseball_test.go
@@ -5,14 +5,18 @@ import "testing"
 type Game interface {
 }
 
+type gameNumbs struct {
+	nums string
+}
+
+func CreateGame(nums string) Game {
+	return &gameNumbs{nums: nums}
+}
+
 func TestCreateGame(t *testing.T) {
 	var game Game = CreateGame("123")
 
 	if game == nil {
 		t.Fatalf("게임 실행 불가")
 	}
-}
-
-func CreateGame(nums string) Game {
-	return nil
 }
