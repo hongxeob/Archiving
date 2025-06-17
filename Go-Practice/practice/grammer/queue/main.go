b0e20ec21a64bc9b785cd25e5f2616b3cc1cdea4
hongxeob <sjun2918@naver.com>
2025-06-17 17:48:34 +0900

    add : golang - queue

---
+++ Go-Practice/practice/grammer/queue/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+type Queue struct {
+	v *list.List
+}
+
+func (q *Queue) Push(val interface{}) {
+	q.v.PushBack(val)
+}
+
+func (q *Queue) Pop() interface{} {
+	front := q.v.Front()
+	if front != nil {
+		return q.v.Remove(front)
+
+	}
+	return nil
+}
+
+func NewQueue() *Queue {
+	return &Queue{list.New()}
+}
+
+func main() {
+	queue := NewQueue()
+
+	for i := 1; i < 5; i++ {
+		queue.Push(i)
+	}
+	value := queue.Pop()
+	for value != nil {
+		fmt.Printf("%v -> ", value)
+		value = queue.Pop()
+	}
+}
