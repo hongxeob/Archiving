4a1898f8e7fb4956a3364638b62b40518c621931
hongxeob <sjun2918@naver.com>
2025-06-19 13:47:48 +0900

    add : fx - 많은 핸들러 등록

--- Go-Practice/fxdemo/main.go
+++ Go-Practice/fxdemo/main.go
@@ -19,22 +19,36 @@ func main() {
 		// fx.Provide()는 fx.New()에 전달된 함수가 fx 애플리케이션의 의존성 주입 컨테이너에 제공될 수 있도록 한다.
 		fx.Provide(
 			NewHTTPServer,
-			NewServeMux,
-			// fx.Annotate()는 fx.Provide()에 전달된 함수에 주석을 추가하여 의존성 주입 컨테이너에 등록할 때 사용할 수 있는 메타데이터를 제공한다.
 			fx.Annotate(
-				NewEchoHandler,
-				fx.As(new(Route)),
-				fx.ResultTags(`name:"echo"`),
+				NewServeMux,
+				// fx.ParamTags()는 fx.Provide()에 전달된 함수의 매개변수에 주석을 추가하여 의존성 주입 컨테이너에서 해당 매개변수를 찾을 때 사용할 수 있는 메타데이터를 제공한다.
+				fx.ParamTags(`group:"routes"`),
 			),
-			fx.Annotate(
-				NewHelloHandler,
-				fx.As(new(Route)),
-				fx.ResultTags(`name:"hello"`)),
+			AsRoute(NewEchoHandler),
+			AsRoute(NewHelloHandler),
+			//fx.Annotate(
+			//	NewEchoHandler,
+			//	fx.As(new(Route)),
+			//	fx.ResultTags(`name:"echo"`),
+			//),
+			//fx.Annotate(
+			//	NewHelloHandler,
+			//	fx.As(new(Route)),
+			//	fx.ResultTags(`name:"hello"`)),
 			zap.NewExample,
 		),
 		// fx.Invoke()는 fx.New()에 전달된 함수를 실행하여 애플리케이션의 생명 주기 동안 특정 시점에 실행될 콜백 함수를 등록한다.
 		fx.Invoke(func(s *http.Server) {}),
 	).Run()
+}
+
+func AsRoute(f any) any {
+	return fx.Annotate(
+		f,
+		fx.As(new(Route)),
+		fx.ResultTags(`group:"routes"`),
+	)
+
 }
 
 // Route 인터페이스는 http.Handler를 구현하는 구조체가 HTTP 요청을 처리할 수 있도록 하는 인터페이스이다.
@@ -77,10 +91,12 @@ func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 // NewServeMux 는 EchoHandler를 사용하여 HTTP 요청을 처리하는 ServeMux를 생성한다.
 // ServeMux는 HTTP 요청을 여러 핸들러로 라우팅하는 데 사용되는 멀티플렉서이다.
-func NewServeMux(route Route) *http.ServeMux {
+func NewServeMux(routes []Route) *http.ServeMux {
 	mux := http.NewServeMux()
 	// HandleFunc()는 HTTP 요청을 처리할 핸들러를 등록한다.
-	mux.Handle(route.Pattern(), route)
+	for _, route := range routes {
+		mux.Handle(route.Pattern(), route)
+	}
 	return mux
 }
 
